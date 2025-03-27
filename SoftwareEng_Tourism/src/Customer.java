cd7f3e2e5391a478cadb4bd6178d1fd9c2dc1256
alexislaz-cse <lazanas.ale@gmail.com>
2025-03-27 08:22:05 +0200

    first commit
    It is just the implementation of the class diagram. Many sections are blank. Data structures that are used are open for change (we may not need so fast searh to add HashSets)

---
+++ SoftwareEng_Tourism/src/Customer.java
@@ -0,0 +1,18 @@
+public class Customer {
+    // To be filled with more attributes and functions
+}
+
+
+class Reservation {
+    public final Customer reserver;
+    public Room reservedRoom;
+    public Voyage voyageRefered;
+
+    // To be filled with more attributes and functions
+
+    Reservation(Customer reserver){
+        this.reserver=reserver;
+    }
+
+    // To be filled with more attributes and functions
+}
