9fd4f6980619576dfc375348c0556aff4ac50aa1
StamyKal <stamykalim@gmail.com>
2025-05-03 18:03:23 +0300

    Delete
    Εχει σβηστει καθως παρατηρηθηκε οτι δεν ανοιγε

--- Instatrip_Java_Impl/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.instatrip_java_impl {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.instatrip_java_impl to javafx.fxml;
-    exports com.example.instatrip_java_impl;
-}
