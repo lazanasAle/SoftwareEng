ee2b801118fdcdd22e82eacec96366078105e62e
Spyros Manolatos <spmanolatos@gmail.com>
2025-05-05 16:20:06 +0300

    If you comment it out should work. I added the 2 classes I forgot to add

---
+++ InstaTripApp/src/main/java/com/example/instatripapp/PackageGUI.java
@@ -0,0 +1,31 @@
+package com.example.instatripapp;
+
+public class PackageGUI {
+    private String id;
+    private String name;
+    private String description;
+    private String price;
+
+    public PackageGUI(String id, String name, String description, String price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+}
