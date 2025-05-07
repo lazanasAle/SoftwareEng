2cf5247ce9ab832d596b2536de4d3606c9644510
Andreas Maroudas <166844756+erikos202@users.noreply.github.com>
2025-05-07 19:02:47 +0300

    SearchPackageScreen

---
+++ InstaTripApp/src/main/java/com/example/instatripapp/SearchPackageScreen.java
@@ -0,0 +1,33 @@
+package com.example;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+public class SearchPackageScreen extends SearchScreen {
+
+
+    public SearchPackageScreen() {
+        super("Αναζήτηση Πακέτων", 1000, 750);
+        renderLabel("Αναζήτηση Πακέτων");
+        renderGrid(500);
+    }
+
+
+    @Override
+    protected void performSearch(String query) {
+        resultsList.getItems().clear();
+        resultsList.setVisible(true); // Show the field when a search is performed
+
+        // dummy αποτελεσματα 
+        if (!query.isBlank()) {
+            resultsList.getItems().addAll(
+                "Πακέτο για Σαντορίνη - 5 μέρες",
+                "Εκδρομή στο Πήλιο με ξεναγό",
+                "Ορεινή Αρκαδία - Περιήγηση"
+            );
+        } else {
+            resultsList.getItems().add("Παρακαλώ εισάγετε κάποιο όρο αναζήτησης.");
+        }
+    }
+}
+
