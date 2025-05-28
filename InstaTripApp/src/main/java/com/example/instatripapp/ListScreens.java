48f318f90e7c5cc873279c159e3e9d2bed46d80e
lazanasAle <lazanas.ale@gmail.com>
2025-05-28 10:23:03 +0300

    Update ListScreens.java

--- InstaTripApp/src/main/java/com/example/instatripapp/ListScreens.java
+++ InstaTripApp/src/main/java/com/example/instatripapp/ListScreens.java
@@ -55,24 +55,6 @@ class ResultScreen extends PackageListScreen {
         });
 
     }
-
-
-
-
-    /*private void renderResultList() {
-        renderLabel("Αποτελέσματα Πακέτων");
-        // Sample data for packages
-        PackageGUI[] packages = {
-                new PackageGUI("1", "Πακέτο 1", "Περιγραφή 1", "100€"),
-                new PackageGUI("2", "Πακέτο 2", "Περιγραφή 2", "200€"),
-                new PackageGUI("3", "Πακέτο 3", "Περιγραφή 3", "300€")
-        };
-
-        String[] columnNames = {"ID", "Όνομα Πακέτου", "Περιγραφή", "Τιμή"};
-        String[] propertyNames = {"id", "name", "description", "price"};
-        String buttonName = "Επιλογή Πακέτου";
-        renderArray(columnNames, Arrays.asList(packages), propertyNames,buttonName, null);
-    }*/
 }
 
 class PackageOptionsScreen extends ListScreen {
